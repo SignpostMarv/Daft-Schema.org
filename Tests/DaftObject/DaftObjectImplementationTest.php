5647eb26dbe51e9ebf67c1edfa81f37d7785bf48
SignpostMarv <me@signpostmarv.name>
2019-03-06 20:04:20 +0000

    adding unit testing

---
+++ Tests/DaftObject/DaftObjectImplementationTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\DaftObject\SchemaOrg\Tests\DaftObject;
+
+use SignpostMarv\DaftObject\SchemaOrg\Tests\DataProviderTrait;
+use SignpostMarv\DaftObject\Tests\DaftObject\DaftObjectImplementationTest as Base;
+
+class DaftObjectImplementationTest extends Base
+{
+    use DataProviderTrait;
+}
---
+++ Tests/DataProviderTrait.php
@@ -0,0 +1,48 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\DaftObject\SchemaOrg\Tests;
+
+use Generator;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use RegexIterator;
+use SignpostMarv\DaftObject\DaftObject;
+
+trait DataProviderTrait
+{
+    /**
+    * @psalm-return Generator<int, array{0:class-string<DaftObject>}, mixed, void>
+    */
+    public function dataProviderImplementations_class_or_interface() : Generator
+    {
+        $root_dir = __DIR__ . '/../src/';
+        $root_ns = 'SignpostMarv\\DaftObject\\SchemaOrg\\';
+
+        $iterator = new RegexIterator(
+            new RecursiveIteratorIterator(new RecursiveDirectoryIterator(
+                $root_dir,
+                (
+                    RecursiveDirectoryIterator::CURRENT_AS_PATHNAME |
+                    RecursiveDirectoryIterator::SKIP_DOTS |
+                    RecursiveDirectoryIterator::UNIX_PATHS
+                )
+            )),
+            ('/^' . preg_quote($root_dir, '/') . '(.+)\.php$/'),
+            RegexIterator::REPLACE
+        );
+        $iterator->replacement = '$1';
+
+        foreach ($iterator as $current) {
+            $current = $root_ns . str_replace('/', '\\', $current);
+
+
+            if (is_a($current, DaftObject::class, true)) {
+                yield [$current];
+            }
+        }
+    }
+}

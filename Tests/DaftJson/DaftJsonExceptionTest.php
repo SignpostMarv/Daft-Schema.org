ec79b9ecbdf72ed028984a09d357fb05930f54a5
SignpostMarv <me@signpostmarv.name>
2019-03-07 18:49:26 +0000

    updating tests

---
+++ Tests/DaftJson/DaftJsonExceptionTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\DaftObject\SchemaOrg\Tests\DaftJson;
+
+use SignpostMarv\DaftObject\SchemaOrg\Tests\DataProviderTrait;
+use SignpostMarv\DaftObject\Tests\DaftJson\DaftJsonExceptionTest as Base;
+
+class DaftJsonExceptionTest extends Base
+{
+    use DataProviderTrait;
+}

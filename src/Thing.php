f7f81ef05c8c9df11ee0d554cdad19c9617b76b0
SignpostMarv <me@signpostmarv.name>
2019-03-10 12:35:41 +0000

    php-cs-fixer

--- src/Thing.php
+++ src/Thing.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 
 namespace SignpostMarv\DaftObject\SchemaOrg;
 
-use BadMethodCallException;
 use InvalidArgumentException;
 use SignpostMarv\DaftObject\AbstractArrayBackedDaftObject;
 use SignpostMarv\DaftObject\DaftJson;

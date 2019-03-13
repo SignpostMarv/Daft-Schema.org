d1ee0260f782d3fe8d206dd07b6db6dae4323b2f
SignpostMarv <me@signpostmarv.name>
2019-03-13 02:23:39 +0000

    refactoring tests

---
+++ Tests/DaftObjectSchemaOrg/CreativeWork/Comment/FuzzingTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\DaftObject\SchemaOrg\Tests\DaftObjectSchemaOrg\CreativeWork\Comment;
+
+use InvalidArgumentException;
+use Generator;
+use ReflectionClassConstant;
+use SignpostMarv\DaftObject\SchemaOrg;
+use SignpostMarv\DaftObject\SchemaOrg\Tests\DataProviderTrait;
+use SignpostMarv\DaftObject\SchemaOrg\Tests\DaftObjectSchemaOrg\DaftObjectFuzzingTest as Base;
+
+class FuzzingTest extends Base
+{
+    protected static function FuzzingImplementationsViaGeneratorRootType() : string
+    {
+        return SchemaOrg\CreativeWork\Comment::class;
+    }
+}
